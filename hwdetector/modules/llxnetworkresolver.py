616cb33a6460ff4b057d95bb03893fe23b49e95c
mijuanlo <m.angel.juan@gmail.com>
2017-10-27 11:39:42 +0200

    Commit

--- hwdetector/modules/llxnetworkresolver.py
+++ hwdetector/modules/llxnetworkresolver.py
@@ -15,14 +15,54 @@ class LlxNetworkResolver(Detector):
     _NEEDS=["LLIUREX_RELEASE","LLIUREX_SESSION_TYPE",'HELPER_CHECK_OPEN_PORT','HELPER_CHECK_NS','LDAP_MODE']
 
     def run(self,*args,**kwargs):
-        release=kwargs['LLIUREX_RELEASE']
-        session=kwargs["LLIUREX_SESSION_TYPE"]
-        ldap_mode=kwargs["LDAP_MODE"]
+        release=kwargs['LLIUREX_RELEASE'].lower()
+        session=kwargs["LLIUREX_SESSION_TYPE"].lower()
+        ldap_mode=kwargs["LDAP_MODE"].lower()
 
-        if release.lower() == 'server' and ldap_mode.lower() == 'slave':
-            print 'im slave'
+        output={'RESOLVED': [] ,'UNRESOLVED':[]}
 
-        return {'RESOLVER_INFO':None}
+        if release == 'server': # SERVERS
+            if ldap_mode == 'slave':
+                for ns in ['server']:
+                    if self.check_ns(ns):
+                        output['RESOLVED'].append(ns)
+                    else:
+                        output['UNRESOLVED'].append(ns)
+            elif ldap_mode == 'independent':
+                for ns in ['server']:
+                    if self.check_ns(ns):
+                        output['RESOLVED'].append(ns)
+                    else:
+                        output['UNRESOLVED'].append(ns)
+            elif ldap_mode == 'master':
+                for ns in ['server','pmb','opac','proxy','owncloud']:
+                    if self.check_ns(ns):
+                        output['RESOLVED'].append(ns)
+                    else:
+                        output['UNRESOLVED'].append(ns)
+            else:
+                for ns in ['server','pmb','opac','proxy','owncloud']:
+                    if self.check_ns(ns):
+                        output['RESOLVED'].append(ns)
+                    else:
+                        output['UNRESOLVED'].append(ns)
+
+        elif release == 'client': # CLIENTS
+            for ns in ['server','pmb','opac','proxy','owncloud']:
+                if self.check_ns(ns):
+                    output['RESOLVED'].append(ns)
+                else:
+                    output['UNRESOLVED'].append(ns)
+
+        else: # OTHERS
+            for ns in []:
+                if self.check_ns(ns):
+                    output['RESOLVED'].append(ns)
+                else:
+                    output['UNRESOLVED'].append(ns)
+
+
+        return {'RESOLVER_INFO': output}
 
 
 
--- hwdetector/modules/llxnetworktest.py
+++ hwdetector/modules/llxnetworktest.py
@@ -11,7 +11,25 @@ class LlxNetworkTest(Detector):
     _PROVIDES=['LLXNETWORK_TEST']
 
     def run(self,*args,**kwargs):
+        msg=[]
+        status=True
+
         netinfo=kwargs['NETINFO']
         resolution=kwargs['RESOLVER_INFO']
-        output={'LLXNETWORK_TEST':{'status':False,'msg':'Network seems down'}}
+
+        # CHECK NETWORK STATUS
+
+
+        # CHECK NAME RESOLUTION
+
+        if resolution['UNRESOLVED']:
+            nr=','.join(resolution['UNRESOLVED'])
+            msg.append(nr+' not reachable')
+            status=False
+        if resolution['RESOLVED']:
+            nr=','.join(resolution['RESOLVED'])
+            msg.append(nr+'Ok! it\'s reachable ')
+
+        msg='\n'.join(msg)
+        output={'LLXNETWORK_TEST':{'status':status,'msg':msg}}
         return output
