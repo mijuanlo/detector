a6d98f0a4ada2874906069be9714a44aadc77698
mijuanlo <m.angel.juan@gmail.com>
2017-10-30 16:55:38 +0100

    Commit Wip

--- hwdetector/modules/llxservices.py
+++ hwdetector/modules/llxservices.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import hwdetector.Detector as Detector
 import utils.log as log
+import subprocess
+import re
 
 log.debug("File "+__name__+" loaded")
 
@@ -11,4 +13,32 @@ class LlxServices(Detector):
     def run(self,*args,**kwargs):
         output={}
 
+        # SYSTEMCTL
+        sysctl_out=subprocess.check_output(['systemctl','--plain','--no-legend','--no-pager','list-units','--all','-t','service'])
+        info={'BYUNIT':{},'BYLOAD':{},'BYACTIVE':{},'BYSUB':{}}
+        for line in sysctl_out.split('\n'):
+            service_info=re.search(r'(?P<UNIT>[\w\-@]+).service\s+(?P<LOAD>\S+)\s+(?P<ACTIVE>\S+)\s+(?P<SUB>\S+)\s+(?P<NAME>.*$)',line)
+            if service_info:
+                d=service_info.groupdict()
+                #info['BYUNIT'].update({d['NAME']:d})
+                for x in ['UNIT','LOAD','ACTIVE','SUB']:
+                    if d[x] in info['BY'+x]:
+                        info['BY'+x][d[x]].append(d)
+                    else:
+                        info['BY'+x][d[x]]=[d]
+
+        output.update({'SYSTEMCTL_INFO':info})
+
+        # APACHE
+        output.update({'APACHE_INFO':None})
+
+
+        # EPOPTES
+        output.update({'EPOPTES_INFO':None})
+
+        # DNSMASQ
+        output.update({'DNSMASQ_INFO':None})
+
+        # SQUID
+        output.update({'SQUID_INFO':None})
         return output
