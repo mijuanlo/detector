666c89c54bf94b27840f67cca909c302f5ad7e85
mijuanlo <m.angel.juan@gmail.com>
2017-11-08 10:39:20 +0100

    Commit Wip

--- hwdetector/modules/llxinitialization.py
+++ hwdetector/modules/llxinitialization.py
@@ -5,18 +5,24 @@ import utils.log as log
 log.debug("File "+__name__+" loaded")
 
 class LlxInitialization(Detector):
-    _NEEDS = ['HELPER_UNCOMMENT','LDAP_MODE']
-    _PROVIDES = ['HOSTNAME','SERVER_LDAP','INTERNAL_INTERFACE','EXTERNAL_INTERFACE']
+    _NEEDS = ['HELPER_UNCOMMENT','LDAP_MODE','HELPER_FILE_FIND_LINE','N4D_VARS']
+    _PROVIDES = ['HOSTNAME','SERVER_LDAP','INTERNAL_INTERFACE','EXTERNAL_INTERFACE','NFS_INITIALIZATION']
 
 
 
     def run(self,*args,**kwargs):
         output = {}
+        ssync_from = None
         n4d_vars=kwargs['N4D_VARS']
         output.update({'HOSTNAME':self.uncomment('/etc/hostname')})
         mapping={'CLIENT_LDAP_URI':'SERVER_LDAP','INTERNAL_INTERFACE':'INTERNAL_INTERFACE','EXTERNAL_INTERFACE':'EXTERNAL_INTERFACE'}
         for search_var in mapping:
             if search_var in n4d_vars:
                 output.update({mapping[search_var]:n4d_vars[search_var]['value']})
+        line=r'What = (\d+(?:\.\d+){3}):/net/server-sync'
+        line=''.join(self.file_find_line(r'/lib/systemd/system/net-server\x2dsync.mount',line))
+        if line:
+            ssync_from=line
+        output.update({'NFS_INITIALIZATION':ssync_from})
 
         return output
--- hwdetector/modules/llxusers.py
+++ hwdetector/modules/llxusers.py
@@ -6,77 +6,153 @@ import os
 log.debug("File "+__name__+" loaded")
 
 class LlxUsers(Detector):
-    _NEEDS = ['HELPER_EXECUTE','HELPER_USERS_LOGGED','LDAP_INFO','MOUNTS_INFO','LLIUREX_SESSION_TYPE','LLIUREX_RELEASE','HELPER_WHO_I_AM','LOGIN_TYPE']
+    _NEEDS = ['HELPER_EXECUTE','HELPER_USERS_LOGGED','LDAP_INFO','MOUNTS_INFO','LLIUREX_SESSION_TYPE','LLIUREX_RELEASE','HELPER_WHO_I_AM','LOGIN_TYPE','LDAP_MODE','NFS_INITIALIZATION']
     _PROVIDES = ['USERS_INFO','USER_TEST']
 
 
     def check_mounts(self,username,typeuser,*args,**kwargs):
         mounts_info=kwargs['MOUNTS_INFO']
-        session_type = kwargs['LLIUREX_SESSION_TYPE']
+        session_type = kwargs['LLIUREX_SESSION_TYPE'].upper()
         release = kwargs['LLIUREX_RELEASE'].lower()
+        ldap_mode = kwargs['LDAP_MODE'].lower()
+        nfs_server = kwargs['NFS_INITIALIZATION']
         ret = None
         msg = []
 
-        if release == 'client':
-            if typeuser == 'student':
-                if session_type == 'FAT' or session_type == 'SEMI':
-                    for share in ['home','share','groups_share']:
-                        mountpoint='/run/' + username + '/' + share
-                        mountsource='//server/'+share
-                        for x in mounts_info['NETWORK']:
-                            ret = False
-                            if x['mount_point'] ==  mountpoint and x['mount_source'] == mountsource and x['fstype'] == 'cifs':
-                                ret = True
-                                msg.append('Samba share {} mounted under {}'.format(mountsource,mountpoint))
-                                break
-                        if not ret:
-                            msg.append('Samba share {} not mounted under {}'.format(mountsource,mountpoint))
-                            break
-                        else:
-                            ret = True
-                    if ret:
-                        bind_paths={'Desktop':'/run/'+username+'/home/students/'+username+'/Desktop','Documents':'/run/'+username+'/home/students/'+username+'/Documents','share':'/run/'+username+'/share','groups_share':'/run/'+username+'/groups_share'}
-                        for bind in bind_paths:
-                            for x in mounts_info['BIND']:
-                                ret = False
-                                if x['mount_point'].startswith('/home/' + username) and x['mount_source'] == bind_paths[bind]:
-                                    ret = True
-                                    msg.append('Bindmount {} from {} available'.format(x['mount_point'],bind_paths[bind]))
-                                    break
-                            if not ret:
-                                msg.append('Bindmount {} from {} not available'.format(bind,bind_paths[bind]))
-                                break
-
-                elif session_type == 'THIN':
-                    pass
-            elif typeuser == 'teacher':
-                pass
-            elif typeuser == 'admin':
-                pass
-        elif release == 'server':
-            if typeuser == 'student':
-                if session_type == 'FAT' or session_type == 'SEMI':
-                    bind_paths={'Desktop':'/net/server-sync/home/students/'+username+'/Desktop','Documents':'/net/server-sync/home/students/'+username+'/Documents','share':'/net/server-sync/share','groups_share':'/net/server-sync/groups_share'}
-                    for bind in bind_paths:
-                        for x in mounts_info['BIND']:
-                            ret = False
-                            if x['mount_point'].startswith('/home/' + username) and x['mount_source'] == bind_paths[bind]:
-                                ret = True
-                                msg.append('Bindmount {} from {} available'.format(x['mount_point'],bind_paths[bind]))
-                                break
-                        if not ret:
-                            msg.append('Bindmount {} from {} not available'.format(bind,bind_paths[bind]))
-                            break
-
-                elif session_type == 'THIN':
-                    pass
-            elif typeuser == 'teacher':
-                pass
-            elif typeuser == 'admin':
-                pass
-        else: # pyme, infantil, desktop, music
+        if release != 'server' and release != 'client':
             return (True,['Not in classroom model'])
+
+        def check_mount_network(username,shares,mounts_info):
+            msgs=[]
+            ret = True
+            for share in shares:
+                mountpoint='/run/' + username + '/' + share
+                mountsource='//server/'+share
+                for x in mounts_info['NETWORK']:
+                    ret = False
+                    if x['mount_point'] ==  mountpoint and x['mount_source'] == mountsource and x['fstype'] == 'cifs':
+                        ret = True
+                        msg.append('Samba share {} mounted under {}'.format(mountsource,mountpoint))
+                        break
+                if not ret:
+                    msgs.append('Samba share {} not mounted under {}'.format(mountsource,mountpoint))
+                    break
+                else:
+                    ret = True
+            return (ret,msgs)
+        def check_binds(username,bind_paths,mounts_info):
+            msgs = []
+            ret = True
+            for bind in bind_paths:
+                for x in mounts_info['BIND']:
+                    ret = False
+                    if x['mount_point'].startswith('/home/' + username) and x['mount_source'] == bind_paths[bind]:
+                        ret = True
+                        msg.append('Bindmount {} from {} available'.format(x['mount_point'],bind_paths[bind]))
+                        break
+                if not ret:
+                    msg.append('Bindmount {} from {} not available'.format(bind,bind_paths[bind]))
+                    break
+            return (ret,msgs)
+
+        samba_shares = []
+        nfs_bind_paths = {}
+        if typeuser == 'student':
+            if release == 'server':
+                nfs_bind_paths={'Desktop':'/net/server-sync/home/students/'+username+'/Desktop','Documents':'/net/server-sync/home/students/'+username+'/Documents','share':'/net/server-sync/share','groups_share':'/net/server-sync/groups_share'}
+            elif release == 'client':
+                if session_type != 'THIN':
+                    samba_shares = ['home','share','groups_share']
+                nfs_bind_paths={'Desktop':'/run/'+username+'/home/students/'+username+'/Desktop','Documents':'/run/'+username+'/home/students/'+username+'/Documents','share':'/run/'+username+'/share','groups_share':'/run/'+username+'/groups_share'}
+        elif typeuser == 'teacher':
+            if release == 'server':
+                nfs_bind_paths={'Desktop':'/net/server-sync/home/teachers/'+username+'/Desktop','Documents':'/net/server-sync/home/teachers/'+username+'/Documents','share':'/net/server-sync/share','groups_share':'/net/server-sync/groups_share','teachers_share':'/net/server-sync/teachers_share','/home/students':'/home/students'}
+            elif release == 'client':
+                if session_type != 'THIN':
+                    samba_shares = ['home','share', 'group_share', 'share_teachers']
+                nfs_bind_paths={'Desktop':'/run/'+username+'/home/teachers/'+username+'/Desktop','Documents':'/run/'+username+'/home/teachers/'+username+'/Documents','share':'/run/'+username+'/share','groups_share':'/run/'+username+'/groups_share','teachers_share':'/run/'+username+'/teachers_share','/home/students':'/home/students'}
+        elif typeuser == 'admin':
+            if release == 'server':
+                nfs_bind_paths={'Desktop':'/net/server-sync/home/admins/'+username+'/Desktop','Documents':'/net/server-sync/home/admins/'+username+'/Documents','share':'/net/server-sync/share','groups_share':'/net/server-sync/groups_share'}
+            elif release == 'client':
+                if session_type != 'THIN':
+                    samba_shares = ['home','share','groups_share','share_teachers']
+                nfs_bind_paths={'Desktop':'/run/'+username+'/home/admins/'+username+'/Desktop','Documents':'/run/'+username+'/home/admins/'+username+'/Documents','share':'/run/'+username+'/share','groups_share':'/run/'+username+'/groups_share'}
+
+        ret, msgs = check_mount_network(username,samba_shares,mounts_info)
+        msg.extend(msgs)
+        if ret:
+            ret,msgs=check_binds(username,nfs_bind_paths,mounts_info)
+            msg.extend(msgs)
+
         return (ret,msg)
+
+        # if release == 'client':
+        #     if typeuser == 'student':
+        #         if session_type == 'FAT' or session_type == 'SEMI':
+        #             shares = ['home','share','groups_share']
+        #             ret, msgs = check_mount_network(username,shares,mounts_info)
+        #             msg.extend(msgs)
+        #             # for share in ['home','share','groups_share']:
+        #             #     mountpoint='/run/' + username + '/' + share
+        #             #     mountsource='//server/'+share
+        #             #     for x in mounts_info['NETWORK']:
+        #             #         ret = False
+        #             #         if x['mount_point'] ==  mountpoint and x['mount_source'] == mountsource and x['fstype'] == 'cifs':
+        #             #             ret = True
+        #             #             msg.append('Samba share {} mounted under {}'.format(mountsource,mountpoint))
+        #             #             break
+        #             #     if not ret:
+        #             #         msg.append('Samba share {} not mounted under {}'.format(mountsource,mountpoint))
+        #             #         break
+        #             #     else:
+        #             #         ret = True
+        #             if ret:
+        #                 bind_paths={'Desktop':'/run/'+username+'/home/students/'+username+'/Desktop','Documents':'/run/'+username+'/home/students/'+username+'/Documents','share':'/run/'+username+'/share','groups_share':'/run/'+username+'/groups_share'}
+        #                 ret,msgs=check_binds(username,bind_paths,mounts_info)
+        #                 msg.extend(msgs)
+        #                 # for bind in bind_paths:
+        #                 #     for x in mounts_info['BIND']:
+        #                 #         ret = False
+        #                 #         if x['mount_point'].startswith('/home/' + username) and x['mount_source'] == bind_paths[bind]:
+        #                 #             ret = True
+        #                 #             msg.append('Bindmount {} from {} available'.format(x['mount_point'],bind_paths[bind]))
+        #                 #             break
+        #                 #     if not ret:
+        #                 #         msg.append('Bindmount {} from {} not available'.format(bind,bind_paths[bind]))
+        #                 #         break
+        #
+        #         elif session_type == 'THIN':
+        #             pass
+        #     elif typeuser == 'teacher':
+        #         pass
+        #     elif typeuser == 'admin':
+        #         pass
+        # elif release == 'server':
+        #     if typeuser == 'student':
+        #         if session_type == 'FAT' or session_type == 'SEMI':
+        #             bind_paths={'Desktop':'/net/server-sync/home/students/'+username+'/Desktop','Documents':'/net/server-sync/home/students/'+username+'/Documents','share':'/net/server-sync/share','groups_share':'/net/server-sync/groups_share'}
+        #             ret,msgs = check_binds(username,bind_paths,mounts_info)
+        #             # for bind in bind_paths:
+        #             #     for x in mounts_info['BIND']:
+        #             #         ret = False
+        #             #         if x['mount_point'].startswith('/home/' + username) and x['mount_source'] == bind_paths[bind]:
+        #             #             ret = True
+        #             #             msg.append('Bindmount {} from {} available'.format(x['mount_point'],bind_paths[bind]))
+        #             #             break
+        #             #     if not ret:
+        #             #         msg.append('Bindmount {} from {} not available'.format(bind,bind_paths[bind]))
+        #             #         break
+        #
+        #         elif session_type == 'THIN':
+        #             pass
+        #     elif typeuser == 'teacher':
+        #         pass
+        #     elif typeuser == 'admin':
+        #         pass
+        # else: # pyme, infantil, desktop, music
+        #     return (True,['Not in classroom model'])
+        #return (ret,msg)
 
     def run(self,*args,**kwargs):
         output={}
